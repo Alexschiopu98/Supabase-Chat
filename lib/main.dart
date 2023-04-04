a6e61b1b03bf76510e52a26e6054fb50820ea7af
Alexschiopu <Alexschiopu98@gmail.com>
2023-04-04 14:14:49 +0100

    dded Splash page and configured Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'pages/splash_page.dart';
+import 'utils/constants.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    // TODO: Replace credentials with your own
+    url: 'SUPABASE_URL',
+    anonKey: 'SUPABASE_ANON_KEY',
+  );
+  runApp(const SupabaseChatApp());
+}
+
+class SupabaseChatApp extends StatelessWidget {
+  const SupabaseChatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Supabase Chat App',
+      theme: appTheme,
+      home: const SplashPage(),
+    );
+  }
+}
